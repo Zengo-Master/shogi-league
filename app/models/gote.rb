e3f72744759ddaede0375ce9363329e1901cb06f
Zengo-Master <8435740@gmail.com>
2021-02-25 14:17:34 +0900

    先手と後手の名前

--- app/models/gote.rb
+++ app/models/gote.rb
@@ -1,15 +1,18 @@
 class Gote < ActiveHash::Base
   self.data = [
     { id: 1,  name: '--' },
-    { id: 2,  name: 'A' },
-    { id: 3,  name: 'B' },
-    { id: 4,  name: 'C' },
-    { id: 5,  name: 'D' },
-    { id: 6,  name: 'E' },
-    { id: 7,  name: 'F' },
-    { id: 8,  name: 'G' },
-    { id: 9,  name: 'H' },
-    { id: 10, name: 'I' },
-    { id: 11, name: 'J' }
+    { id: 2,  name: 'すぎうら' },
+    { id: 3,  name: 'ぷく' },
+    { id: 4,  name: 'ほっしー' },
+    { id: 5,  name: 'あつと' },
+    { id: 6,  name: 'しゅん' },
+    { id: 7,  name: 'しゅうまい' },
+    { id: 8,  name: 'TK' },
+    { id: 9,  name: 'こいし' },
+    { id: 10, name: 'れおす' },
+    { id: 11, name: 'カイト' },
+    { id: 12, name: 'へいや' },
+    { id: 13, name: 'ちょうの' },
+    { id: 14, name: 'きふわるべ' }
   ]
 end
