cbdaf1dfdd828b95482b2e9f994e5508512a9340
Zengo-Master <8435740@gmail.com>
2021-06-15 00:08:27 +0900

    斜線

--- app/models/sente.rb
+++ app/models/sente.rb
@@ -7,8 +7,6 @@ class Sente < ActiveHash::Base
     { id: 5,  name: 'あつと' },
     { id: 6,  name: 'しゅん' },
     { id: 7,  name: 'しゅうまい' },
-    # { id: 8,  name: 'TK' },
-    # { id: 9,  name: 'こいし' },
     { id: 8, name: 'れおす' },
     { id: 9, name: 'カイト' },
     { id: 10, name: 'へいや' },
